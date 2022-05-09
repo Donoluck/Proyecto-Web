cc08ae0eaacf278627a4ea893df8b16fcd2face6
Donovan <102259269+Donoluck@users.noreply.github.com>
2022-05-08 22:15:15 -0400

    Add files via upload

---
+++ Proyectoweb_version2/js/Historia.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#boton1").click(function () { 
+        $("#parrafo_1").toggle("slow");;
+    });
+
+    $("#boton2").click(function () { 
+        $("#parrafo2").toggle("slow");;
+    });
+
+    $("#boton3").click(function () { 
+        $("#parrafo3").toggle("slow");;
+    });
+});
